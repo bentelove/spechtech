04bbb58e7e61b6d2113e481a2bac842850e0cbba
bentelove <bent.worker@yandex.ru>
2026-01-10 19:26:30 +0300

    test2

--- lib/telegram/client/TelegramInit.tsx
+++ lib/telegram/client/TelegramInit.tsx
@@ -9,5 +9,10 @@ export default function TelegramInit() {
       localStorage.setItem('token', rawInitData);
     }
   }, [rawInitData]);
+  if(rawInitData){
+    return <>
+      <pre>{JSON.stringify(rawInitData, null, 2)}</pre>
+    </>;
+  }
   return null;
 }
